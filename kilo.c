6849cbc56deeded78c524e8d8d318d7e21d7192c
Jordan Ayotte <jordanayotte13@outlook.com>
2025-06-14 22:36:32 -0400

    control v disbaled

--- kilo.c
+++ kilo.c
@@ -15,7 +15,8 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_iflag &= ~(IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -30,7 +31,7 @@ int main() {
         }
         else {
             printf("%d ('%c')\n", c, c);
-        }
+        } 
     }
     return 0;
 }
