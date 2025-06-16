7ba1c69dfdb1ea8d99f2c391a579a8741619fabc
Jordan Ayotte <jordanayotte13@outlook.com>
2025-06-16 10:08:29 -0400

    clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -18,8 +18,11 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
-  perror(s);
-  exit(1);
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    perror(s);
+    exit(1);
 }
 
 void disableRawMode() {
@@ -65,6 +68,8 @@ void editorProcessKeypress() {
 
     switch(c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
