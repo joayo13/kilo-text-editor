50f36903be7dae2857834f09a392cafda6962302
Jordan Ayotte <jordanayotte13@outlook.com>
2025-07-19 18:20:47 -0400

    quit confirmation

--- kilo.c
+++ kilo.c
@@ -24,6 +24,8 @@
 
 #define KILO_TAB_STOP 8
 
+#define KILO_QUIT_TIMES 3
+
 enum editorKey {
     BACKSPACE = 127,
     ARROW_LEFT = 1000,
@@ -485,12 +487,21 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+    static int quit_times = KILO_QUIT_TIMES;
     int c = editorReadKey();
     switch (c) {
         case '\r':
             /* TODO */
             break;
         case CTRL_KEY('q'):
+            if (E.dirty && quit_times > 0) {
+                editorSetStatusMessage(
+                    "WARNING!!! File has unsaved changes. "
+                    "Press Ctrl-Q %d more times to quit.",
+                    quit_times);
+                quit_times--;
+                return;
+            }
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
@@ -534,6 +545,7 @@ void editorProcessKeypress() {
             editorInsertChar(c);
             break;
     }
+    quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
