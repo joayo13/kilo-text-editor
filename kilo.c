a61c2e6d8d1be55216ea44053fd6f6a0ba977be6
Jordan Ayotte <jordanayotte13@outlook.com>
2025-08-05 13:36:32 -0400

    colorful search results

--- kilo.c
+++ kilo.c
@@ -39,7 +39,7 @@ enum editorKey {
     PAGE_DOWN
 };
 
-enum editorHighlight { HL_NORMAL = 0, HL_NUMBER };
+enum editorHighlight { HL_NORMAL = 0, HL_NUMBER, HL_MATCH };
 
 /*** data ***/
 typedef struct erow {
@@ -215,6 +215,8 @@ int editorSyntaxToColor(int hl) {
     switch (hl) {
         case HL_NUMBER:
             return 31;
+        case HL_MATCH:
+            return 34;
         default:
             return 37;
     }
@@ -459,6 +461,7 @@ void editorFindCallback(char* query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
